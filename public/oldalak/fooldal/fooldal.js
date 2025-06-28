70ce92f65ba5c73f6b8ff277538da51bffa0ab9a
SzucsDani1 <szucsdaniel1@gmail.com>
2025-06-28 21:53:41 +0200

    Kártyákon és a modal-okon minden adat megjelenik dizájnosan

--- public/oldalak/fooldal/fooldal.js
+++ public/oldalak/fooldal/fooldal.js
@@ -1,152 +1,22 @@
-let termekek = [
-  {
-    id: 0,
-    nev: "Alma",
-    ar: 300,
-    kep: "images/alma.jpg",
-    leiras: "Az alma az egyik legnépszerűbb gyümölcs hazánkban. Frissítő, lédús, vitaminokban gazdag, és remekül fogyasztható önmagában vagy süteményekben."
-  },
-  {
-    id: 1,
-    nev: "Banán",
-    ar: 400,
-    kep: "images/banan.jpg",
-    leiras: "A banán édes, puha húsú gyümölcs, amely kiváló energiaforrás. Gyorsan eltelít, ideális tízóraira vagy uzsonnára."
-  },
-  {
-    id: 2,
-    nev: "Narancs",
-    ar: 350,
-    kep: "images/narancs.jpg",
-    leiras: "A narancs C-vitaminban gazdag citrusféle, amely frissítő ízével tökéletes választás reggelire vagy gyümölcslé készítéséhez."
-  },
-  {
-    id: 3,
-    nev: "Paradicsom",
-    ar: 250,
-    kep: "images/paradicsom.jpg",
-    leiras: "A paradicsom sokoldalúan felhasználható zöldség, amely saláták, szendvicsek és főételek elengedhetetlen alapanyaga."
-  },
-  {
-    id: 4,
-    nev: "Uborka",
-    ar: 200,
-    kep: "images/uborka.jpg",
-    leiras: "Az uborka frissítő, ropogós zöldség, amely kiválóan alkalmas salátákba vagy önmagában fogyasztva is."
-  },
-  {
-    id: 5,
-    nev: "Káposzta",
-    ar: 150,
-    kep: "images/kaposzta.jpg",
-    leiras: "A káposzta egész évben elérhető, vitaminokban és rostokban gazdag zöldség, amely savanyítva vagy főzve is finom."
-  },
-  {
-    id: 6,
-    nev: "Sárgarépa",
-    ar: 180,
-    kep: "images/sargarepa.jpg",
-    leiras: "A sárgarépa édeskés ízű, ropogós zöldség, amely remekül fogyasztható nyersen, főzve vagy levesekben."
-  },
-  {
-    id: 7,
-    nev: "Körte",
-    ar: 320,
-    kep: "images/korle.jpg",
-    leiras: "A körte lédús, édes gyümölcs, amely kiválóan alkalmas desszertekhez vagy önmagában fogyasztva is."
-  },
-  {
-    id: 8,
-    nev: "Szőlő",
-    ar: 450,
-    kep: "images/szolo.jpg",
-    leiras: "A szőlő apró, lédús gyümölcs, amelyet frissen, aszalva vagy bor formájában is élvezhetünk."
-  },
-  {
-    id: 9,
-    nev: "Eper",
-    ar: 500,
-    kep: "images/eper.jpg",
-    leiras: "Az eper illatos, zamatos gyümölcs, amely tökéletes süteményekhez, turmixokhoz vagy önmagában fogyasztva."
-  },
-  {
-    id: 10,
-    nev: "Cseresznye",
-    ar: 600,
-    kep: "images/cseresznye.jpg",
-    leiras: "A cseresznye édes, lédús gyümölcs, amely a nyár egyik legkedveltebb finomsága."
-  },
-  {
-    id: 11,
-    nev: "Kivi",
-    ar: 400,
-    kep: "images/kivi.jpg",
-    leiras: "A kivi egzotikus gyümölcs, amely magas C-vitamin tartalmával és friss ízével egészséges választás."
-  },
-  {
-    id: 12,
-    nev: "Paprika",
-    ar: 220,
-    kep: "images/paprika.jpg",
-    leiras: "A paprika színes, ropogós zöldség, amely salátákban, főételekben vagy nyersen is kitűnő."
-  },
-  {
-    id: 13,
-    nev: "Hagyma",
-    ar: 130,
-    kep: "images/hagyma.jpg",
-    leiras: "A hagyma alapvető konyhai hozzávaló, amely ízesíti az ételeket, és számos egészségügyi előnnyel bír."
-  },
-  {
-    id: 14,
-    nev: "Fokhagyma",
-    ar: 170,
-    kep: "images/fokhagyma.jpg",
-    leiras: "A fokhagyma intenzív ízű fűszernövény, amely nemcsak finom, hanem egészséges is."
-  },
-  {
-    id: 15,
-    nev: "Cukkini",
-    ar: 210,
-    kep: "images/cukkini.jpg",
-    leiras: "A cukkini könnyen emészthető, sokoldalú zöldség, amely grillezve, párolva vagy rakott ételekben is kiváló."
-  },
-  {
-    id: 16,
-    nev: "Ananász",
-    ar: 550,
-    kep: "images/ananasz.jpg",
-    leiras: "Az ananász trópusi gyümölcs, amely édes és savanykás ízével frissítő csemege."
-  },
-  {
-    id: 17,
-    nev: "Málna",
-    ar: 480,
-    kep: "images/malna.jpg",
-    leiras: "A málna apró, lédús bogyós gyümölcs, amely kitűnő süteményekhez, lekvárokhoz vagy önmagában is."
-  },
-  {
-    id: 18,
-    nev: "Káposzta",
-    ar: 150,
-    kep: "images/kaposzta.jpg",
-    leiras: "A káposzta egész évben elérhető, vitaminokban és rostokban gazdag zöldség, amely savanyítva vagy főzve is finom."
-  },
-  {
-    id: 19,
-    nev: "Citrom",
-    ar: 300,
-    kep: "images/citrom.jpg",
-    leiras: "A citrom savanykás, frissítő citrusféle, amely italok, teák és ételek ízesítésére is kiváló."
+async function osszesTermekLeker(){
+  try {
+    let leker = await fetch("/api/osszestermek");
+    
+    if (leker.ok) {
+      let termekek = await leker.json();
+      termekekBetoltes(termekek);
+      modalMegjelenit(termekek);
+    }
+    else {
+      console.log(leker.status);
+    }
+  } catch (error) {
+    console.log(error);
   }
-]
+}
 
 
-
-
-
-
-function termekekBetoltes(){
+function termekekBetoltes(termekek){
     divContainer = document.getElementById("termekek");  
 
     let divRow = document.createElement("div");
@@ -162,7 +32,7 @@ function termekekBetoltes(){
       let divCard = document.createElement("div");
         divCard.classList = "card col-12 col-lg-3 col-md-6 col-sm-12 p-2 mx-auto my-3"; 
         divCard.style = "width: 18rem;";
-        divCard.id = termek.nev;
+        divCard.id = termek.termek;
 
         let div = document.createElement("div");
         div.classList = "mx-auto"
@@ -170,9 +40,9 @@ function termekekBetoltes(){
         div.style.width = "250px"
 
         let img = document.createElement("img");
-        img.src = "../termekfeltolto/adatbazisInterakciok/"+termek.kepek;
+        img.src = "../.."+termek.kepURL;
         img.classList = "card-img-top";
-        img.alt = termek.nev;
+        img.alt = termek.termek;
         img.style.maxWidth = "250px";
         img.style.maxHeight = "230px";
 
@@ -180,21 +50,29 @@ function termekekBetoltes(){
         divCardBody.classList = "card-body text-center";
 
         let h5 = document.createElement("h5");
-        h5.classList = "card-title";
-        h5.innerHTML = termek.nev;
+        h5.classList = "card-title display-6";
+        h5.innerHTML = termek.termek;
 
         let cardText = document.createElement("p");
         cardText.classList = "card-text";
         cardText.innerHTML = termek.leiras;
 
-        let br = document.createElement("br");
+        let ul = document.createElement("ul");
+        ul.classList = "list-group list-group-horizontal justify-content-center my-3";
 
-        
+        let liAr = document.createElement("li");
+        liAr.classList = "list-group-item";
+        liAr.innerHTML = termek.ar+" Ft";
+
+        let liMertekegyseg = document.createElement("li");
+        liMertekegyseg.classList = "list-group-item";
+        liMertekegyseg.innerHTML = termek.kiszereles+" "+termek.mertekegyseg;
+
         let btnReszletek = document.createElement("input");
         btnReszletek.type = "button";
         btnReszletek.classList = "btn btn-info mb-3";
         btnReszletek.value = "Részletek";
-        btnReszletek.id = "btn"+termek.nev;
+        btnReszletek.id = "btn"+termek.termek;
         btnReszletek.setAttribute("data-bs-toggle", "modal");
         btnReszletek.setAttribute("data-bs-target", "#modal"+termek.id);
 
@@ -203,7 +81,7 @@ function termekekBetoltes(){
         btnKorsarba.type = "button";
         btnKorsarba.classList = "btn btn-primary w-100";
         btnKorsarba.value = "Kosárba";
-        btnKorsarba.id = "btn"+termek.nev;
+        btnKorsarba.id = "btn"+termek.termek;
 
         divRow.appendChild(divCard);
 
@@ -213,7 +91,9 @@ function termekekBetoltes(){
 
         divCardBody.appendChild(h5);
         divCardBody.appendChild(cardText)
-        divCardBody.appendChild(br);
+        divCardBody.appendChild(ul);
+        ul.appendChild(liAr);
+        ul.appendChild(liMertekegyseg);
         divCardBody.appendChild(btnReszletek);
         divCardBody.appendChild(btnKorsarba);
 
@@ -223,7 +103,7 @@ function termekekBetoltes(){
 }
 
 
-async function modalMegjelenit(){
+function modalMegjelenit(termekek){
   foDiv = document.getElementById("termekek");  
 
   for(let termek of termekek){
@@ -240,15 +120,15 @@ async function modalMegjelenit(){
     divModalDialog.role = "document";
 
     let divModalContent = document.createElement("div");
-    divModalContent.classList = "modal-content";
+    divModalContent.classList = "modal-content text-center";
     
     let divModalHeader = document.createElement("div");
     divModalHeader.classList = "modal-header";
 
     let h5ModalTitle = document.createElement("h5");
-    h5ModalTitle.classList = "modal-title";
+    h5ModalTitle.classList = "modal-title mx-auto";
     h5ModalTitle.id = "myModalLabel";
-    h5ModalTitle.innerHTML = termek.nev;
+    h5ModalTitle.innerHTML = termek.termek;
 
     let btnClose = document.createElement("button");
     btnClose.type = "button";
@@ -256,19 +136,42 @@ async function modalMegjelenit(){
     btnClose.setAttribute("data-bs-dismiss", "modal");
     btnClose.setAttribute("aria-label", "Bezárás");
 
-    let spanClose = document.createElement("span");
-    spanClose.setAttribute("aria-hidden", "true");
-    spanClose.innerHTML = "&times;";
+    let img = document.createElement("img");
+    img.src = "../.."+termek.kepURL;
+    img.classList = "card-img-top mx-auto mt-3 rounded";
+    img.alt = termek.termek;
+    img.style.maxWidth = "300px";
+    img.style.maxHeight = "300px";
 
     let divModalBody = document.createElement("div");
     divModalBody.classList = "modal-body";
     divModalBody.innerHTML = termek.leiras;
 
+    let ul = document.createElement("ul");
+    ul.classList = "list-group list-group-horizontal justify-content-center my-3";
+
+    let liAr = document.createElement("li");
+    liAr.classList = "list-group-item";
+    liAr.innerHTML = termek.ar+" Ft";
+
+    let liMertekegyseg = document.createElement("li");
+    liMertekegyseg.classList = "list-group-item";
+    liMertekegyseg.innerHTML = termek.kiszereles+" "+termek.mertekegyseg;
+
     let divModalFooter = document.createElement("div");
     divModalFooter.classList = "modal-footer";
 
+    let div = document.createElement("div");
+    div.classList = "mx-auto";
+
+    let btnKorsarba = document.createElement("input");
+    btnKorsarba.type = "button";
+    btnKorsarba.classList = "btn btn-primary me-2";
+    btnKorsarba.value = "Kosárba";
+    btnKorsarba.id = "btn"+termek.termek;
+
     let btnBezar = document.createElement("button");
-    btnBezar.classList = "btn btn-secondary";
+    btnBezar.classList = "btn btn-secondary ms-2";
     btnBezar.setAttribute("data-bs-dismiss", "modal");
     btnBezar.innerHTML = "Bezár";
 
@@ -278,12 +181,18 @@ async function modalMegjelenit(){
 
     divModalContent.appendChild(divModalHeader);
     divModalHeader.appendChild(h5ModalTitle);
-    divModalHeader.appendChild(btnClose);
 
+    divModalContent.appendChild(img);
     divModalContent.appendChild(divModalBody);
 
+    divModalContent.appendChild(ul);
+    ul.appendChild(liAr);
+    ul.appendChild(liMertekegyseg);
+
     divModalContent.appendChild(divModalFooter);
-    divModalFooter.appendChild(btnBezar);
+    divModalFooter.appendChild(div);
+    div.appendChild(btnKorsarba);
+    div.appendChild(btnBezar);
   }
   
 }
@@ -291,6 +200,6 @@ async function modalMegjelenit(){
 
 
 window.addEventListener("load", function(){
-  termekekBetoltes();
-  modalMegjelenit();
+  osszesTermekLeker();
+
 });
