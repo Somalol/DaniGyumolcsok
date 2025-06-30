34374c0885f7fdd9e23805fc1778be35446bef90
SzucsDani1 <szucsdaniel1@gmail.com>
2025-06-30 13:48:37 +0200

    Navbar kijavítása és a kártyákon ha rányomunk a kosár gombra, hozzáadódik az adott termék id-ja és a mennyisége a local storage-hoz és megjelenik a kosárban is

--- public/oldalak/fooldal/fooldal.js
+++ public/oldalak/fooldal/fooldal.js
@@ -1,11 +1,14 @@
+var termekek = [];
+
 async function osszesTermekLeker(){
   try {
     let leker = await fetch("/api/osszestermek");
     
     if (leker.ok) {
-      let termekek = await leker.json();
-      termekekBetoltes(termekek);
-      modalMegjelenit(termekek);
+      termekek = await leker.json();
+      termekekBetoltes();
+      modalMegjelenit();
+      kosarBetolt();
     }
     else {
       console.log(leker.status);
@@ -16,7 +19,7 @@ async function osszesTermekLeker(){
 }
 
 
-function termekekBetoltes(termekek){
+function termekekBetoltes(){
     divContainer = document.getElementById("termekek");  
 
     let divRow = document.createElement("div");
@@ -83,7 +86,7 @@ function termekekBetoltes(termekek){
         btnKorsarba.value = "Kosárba";
         btnKorsarba.id = "btn"+termek.termek;
         btnKorsarba.addEventListener("click", function(){
-          //kosarMegjelen();
+          termekKosarba(termek.id);
         })
 
         divRow.appendChild(divCard);
@@ -106,7 +109,7 @@ function termekekBetoltes(termekek){
 }
 
 
-function modalMegjelenit(termekek){
+function modalMegjelenit(){
   foDiv = document.getElementById("termekek");  
 
   for(let termek of termekek){
@@ -200,80 +203,134 @@ function modalMegjelenit(termekek){
   
 }
 
-//TODO
-function kosarMegjelen(){
-    foDiv = document.getElementById("termekek");  
+function kosarBetolt(){
+  foDiv = document.getElementById("termekek");  
 
-  for(let termek of termekek){
-    let divModalFade = document.createElement("div");
-    divModalFade.classList = "modal fade";
-    divModalFade.id = "modal"+termek.id;
-    divModalFade.tabIndex = "-1";
-    divModalFade.role = "dialog";
-    divModalFade.setAttribute("aria-lebelledby", "myModalLabel");
-    divModalFade.ariaHidden = "true";
+  let divModalFade = document.createElement("div");
+  divModalFade.classList = "modal fade";
+  divModalFade.id = "kosarModal";
+  divModalFade.tabIndex = "-1";
+  divModalFade.role = "dialog";
+  divModalFade.setAttribute("aria-lebelledby", "myModalLabel");
+  divModalFade.ariaHidden = "true";
 
-    let divModalDialog = document.createElement("div");
-    divModalDialog.classList = "modal-dialog";
-    divModalDialog.role = "document";
+  let divModalDialog = document.createElement("div");
+  divModalDialog.classList = "modal-dialog";
+  divModalDialog.role = "document";
 
-    let divModalContent = document.createElement("div");
-    divModalContent.classList = "modal-content text-center";
-    
-    let divModalHeader = document.createElement("div");
-    divModalHeader.classList = "modal-header";
+  let divModalContent = document.createElement("div");
+  divModalContent.classList = "modal-content text-center";
+  divModalContent.id = "kosarBetoltModalContent";
 
-    let h5ModalTitle = document.createElement("h5");
-    h5ModalTitle.classList = "modal-title mx-auto";
-    h5ModalTitle.id = "myModalLabel";
-    h5ModalTitle.innerHTML = "Kosár";
-
-    let btnClose = document.createElement("button");
-    btnClose.type = "button";
-    btnClose.classList = "close";
-    btnClose.setAttribute("data-bs-dismiss", "modal");
-    btnClose.setAttribute("aria-label", "Bezárás");
-
-
-    let divModalBody = document.createElement("div");
-    divModalBody.classList = "modal-body";
-    divModalBody.innerHTML = termek.leiras;
-
-
-    let divModalFooter = document.createElement("div");
-    divModalFooter.classList = "modal-footer";
-
-    let div = document.createElement("div");
-    div.classList = "mx-auto";
-
-    let btnVasarlas = document.createElement("input");
-    btnVasarlas.type = "button";
-    btnVasarlas.classList = "btn btn-primary me-2";
-    btnVasarlas.value = "Vásárlás";
-
-    let btnBezar = document.createElement("button");
-    btnBezar.classList = "btn btn-secondary ms-2";
-    btnBezar.setAttribute("data-bs-dismiss", "modal");
-    btnBezar.innerHTML = "Bezár";
-
-    foDiv.appendChild(divModalFade);
-    divModalFade.appendChild(divModalDialog);
-    divModalDialog.appendChild(divModalContent);
-
-    divModalContent.appendChild(divModalHeader);
-    divModalHeader.appendChild(h5ModalTitle);
-
-    divModalContent.appendChild(divModalBody);
-
-    divModalContent.appendChild(divModalFooter);
-    divModalFooter.appendChild(div);
-    div.appendChild(btnVasarlas);
-    div.appendChild(btnBezar);
-  }
+  foDiv.appendChild(divModalFade);
+  divModalFade.appendChild(divModalDialog);
+  divModalDialog.appendChild(divModalContent);
 }
 
+function termekKosarba(termekId) {
+    let kosar = JSON.parse(localStorage.getItem("kosar")) || [];
+    let adottTermekAKosarbanVanE = kosar.findIndex(x => x.id === termekId);
+
+    if (adottTermekAKosarbanVanE != -1) {
+        kosar[adottTermekAKosarbanVanE].mennyiseg += 1;
+    } else {
+        kosar.push({ id: termekId, mennyiseg: 1 });
+    }
+
+    localStorage.setItem("kosar", JSON.stringify(kosar));
+}
+
+
+
+function kosarMegjelen(){
+  let divModalContent = document.getElementById("kosarBetoltModalContent");
+  divModalContent.innerHTML = "";
+
+  let kosar = JSON.parse(localStorage.getItem("kosar")) || [];
+
+  let divModalBody = document.createElement("div");
+  divModalBody.classList = "modal-body";
+
+  let divModalHeader = document.createElement("div");
+  divModalHeader.classList = "modal-header";
+
+  let h5ModalTitle = document.createElement("h5");
+  h5ModalTitle.classList = "modal-title mx-auto";
+  h5ModalTitle.id = "myModalLabel";
+  h5ModalTitle.innerHTML = "Kosár";  
+
+  let btnClose = document.createElement("button");
+  btnClose.type = "button";
+  btnClose.classList = "close";
+  btnClose.setAttribute("data-bs-dismiss", "modal");
+  btnClose.setAttribute("aria-label", "Bezárás");
+
+  let divModalFooter = document.createElement("div");
+  divModalFooter.classList = "modal-footer";
+
+  let div = document.createElement("div");
+  div.classList = "mx-auto";
+
+  let btnVasarlas = document.createElement("input");
+  btnVasarlas.type = "button";
+  btnVasarlas.classList = "btn btn-primary me-2";
+  btnVasarlas.value = "Vásárlás";
+
+  let btnBezar = document.createElement("button");
+  btnBezar.classList = "btn btn-secondary ms-2";
+  btnBezar.setAttribute("data-bs-dismiss", "modal");
+  btnBezar.innerHTML = "Bezár";
+
+  divModalContent.appendChild(divModalHeader);
+  divModalHeader.appendChild(h5ModalTitle);
+
+  let ul = document.createElement("ul");
+  ul.classList = "list-group mx-3 my-3";
+  console.log(kosar);
+
+  if(kosar == ""){
+    let alert = document.createElement("div");
+    alert.classList = "alert alert-warning text-center";
+    alert.role = "alert";
+    alert.innerHTML = "Nincs termék a kosárban!";
+
+    divModalContent.appendChild(divModalBody);
+    divModalBody.appendChild(alert);
+  }
+  else{
+    for(let index of kosar){
+      let li = document.createElement("li");
+      li.classList = "list-group-item";
+      
+      let adottTermek = termekek.find(x => x.id == index.id);
+      let label = document.createElement("span");
+      label.innerHTML = adottTermek.termek + " - " + adottTermek.ar + " Ft / " + adottTermek.kiszereles + " " + adottTermek.mertekegyseg;
+
+      let btnTermekTorles = document.createElement("button");
+      btnTermekTorles.type = "button";
+      btnTermekTorles.value = "Törlés";
+      btnTermekTorles.classList = "btn btn-danger btn-sm";
+      btnTermekTorles.innerHTML = "Törlés";
+      btnTermekTorles.style.float = "right";
+
+      ul.appendChild(li);
+      li.appendChild(label);
+      li.appendChild(btnTermekTorles);
+    }
+  }
+
+  divModalContent.appendChild(ul)
+  divModalContent.appendChild(divModalFooter);
+
+  divModalFooter.appendChild(div);
+  div.appendChild(btnVasarlas);
+  div.appendChild(btnBezar);
+
+}
 
 window.addEventListener("load", function(){
   osszesTermekLeker();
 
 });
+
+document.getElementById("btnKosarMegtekint").addEventListener("click", kosarMegjelen);
